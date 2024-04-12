2a651feff675522c16206094f21be765a4406bc5
Ornyx <cthulhu345@gmail.com>
2024-04-12 15:14:57 +1200

    Added exposed methods to change the active player
    Also rearranged some stuff and like switched out the map for a list for easier
    indexing.
    Also did some various cleanups

--- main.go
+++ main.go
@@ -10,19 +10,51 @@ import (
 	"strings"
 
 	"github.com/godbus/dbus/v5"
+	"github.com/godbus/dbus/v5/introspect"
 )
+
+const intro = `
+<node>
+	<interface name="com.ornyx.MprisWatcher">
+   		<method name="Next">
+   		</method>
+   		<method name="Prev">
+   		</method>
+   	</interface>` + introspect.IntrospectDataString + `</node> `
 
 type player struct {
 	dbus.BusObject
 	name string
 }
 
+// Next go to the next active mediaplayer
+func (p *player) Next() *dbus.Error {
+	if len(PlayerList) == 0 {
+		PlayerNum = 0
+		return nil
+	}
+	
+	PlayerNum = (PlayerNum+1) % len(PlayerList)
+	updateMediaMap()
+	return nil
+}
+
+// Prev go to the previous active mediaplayer
+func (p *player) Prev() *dbus.Error {
+	if len(PlayerList) == 0 {
+		PlayerNum = 0
+		return nil
+	}
+	
+	PlayerNum = (PlayerNum + len(PlayerList) - 1) % len(PlayerList)
+	updateMediaMap()
+	return nil
+}
+
 var Conn *dbus.Conn
-var PlayerMap = make(map[string]*player)
-
+var PlayerList []*player
 var ActivePlayer *player
-
-const ClearLine = "\033[2K\r"
+var PlayerNum = 0 // the number in PlayerList to use
 
 // the play pause constants - TODO - maybe replace these with symbols from like a config file or smth
 const (
@@ -52,9 +84,15 @@ func init() {
 	if err != nil {
 		failFunc(fmt.Errorf("Failed to initialize the match signals: %v", err))
 	}
+
+	err = initMethodExport()
+	if err != nil {
+		failFunc(err)
+	}
 }
 
 // initMatchSignals initialize the match signals
+// TODO - make it only match mediaplayer signals
 func initMatchSignals() error {
 	err := Conn.AddMatchSignal(
 		dbus.WithMatchSender("org.freedesktop.DBus"))
@@ -71,6 +109,23 @@ func initMatchSignals() error {
 	return nil
 }
 
+// initMethodExport initialize the method exports for the Player Object
+func initMethodExport() error {
+	Conn.Export(ActivePlayer, "/com/ornyx/MprisWatcher", "com.ornyx.MprisWatcher")
+	Conn.Export(introspect.Introspectable(intro), "/com/ornyx/MprisWatcher",
+		"org.freedesktop.DBus.Introspectable")
+
+	reply, err := Conn.RequestName("com.ornyx.MprisWatcher", dbus.NameFlagDoNotQueue)
+	if err != nil {
+		return err
+	}
+	if reply != dbus.RequestNameReplyPrimaryOwner {
+		return fmt.Errorf("Name already taken, do you have multiple of this program open?")
+	}
+
+	return nil
+}
+
 func main() {
 	defer Conn.Close()
 
@@ -82,19 +137,22 @@ func main() {
 		dbusName, identity, playbackStatus, title, shouldchange := stepProg(c)
 		if shouldchange {	
 			fmt.Printf("%s\x1f%s\x1f%s\x1f%s\n", dbusName, identity, playbackStatus, title)
-			// fmt.Printf("%s %s %s %s\n", dbusName, identity, playbackStatus, title)
+			// for _, p := range PlayerList {
+			// 	fmt.Println(p.GetPlayerInfo())
+			// }
+
+			// fmt.Println(PlayerNum)
+			// fmt.Println()
 		}
 	}
 }
 
-// stepProg step the program once (needed for testing)
-// TODO - maybe make it return a string instead of print one
+// stepProg step the program once and return the player information
 func stepProg(c chan *dbus.Signal) (dbusName, niceName, playbackStatus, title string, shouldChange bool) {
 	select {
 	case s := <-c:
-
 		if s != nil && s.Body != nil {
-			if s.Path == "/org/freedesktop/Notifications" {
+			if s.Path == "/org/freedesktop/Notifications" { // don't update now
 				return
 			}
 
@@ -107,20 +165,12 @@ func stepProg(c chan *dbus.Signal) (dbusName, niceName, playbackStatus, title st
 		// fmt.Printf("\n%+v  |  ", s)
 
 		if ActivePlayer == nil {
-			// fmt.Printf("%sNo media devices", ClearLine)
 			shouldChange = true
 			return
 		}
 		
-	
-		// if err != nil {
-		// 	// fmt.Printf("%sNo media playing", ClearLine)
-		// 	return
 		track, playbackStatus, identity, _ := ActivePlayer.GetPlayerInfo()
 		return ActivePlayer.name, identity, playbackStatus, track, true
-
-		// fmt.Printf("%s%s %s", ClearLine, playbackStatus, metadata)
-		// fmt.Println(metadata)
 	}
 }
 
@@ -130,7 +180,7 @@ func updateMediaMap() error {
 	var s []string
 	var err error
 	var play *player
-	var newPlayerMap = make(map[string]*player)
+	var newPlayerList = []*player{}
 
 	err = Conn.BusObject().Call("org.freedesktop.DBus.ListNames", 0).Store(&s) // get all the objects
 	if err != nil {
@@ -140,26 +190,23 @@ func updateMediaMap() error {
 
 	for _, v := range s {
 		if strings.Contains(v, "MediaPlayer2") {
-
 			play = &player{BusObject: Conn.Object(v, "/org/mpris/MediaPlayer2"), name: v}
-			newPlayerMap[v] = play
+			newPlayerList =  append(newPlayerList, play)
 
 			if ActivePlayer == nil {
-				ActivePlayer = newPlayerMap[v]
+				ActivePlayer = newPlayerList[0]
 			}
 		}
 	}
 
-	PlayerMap = newPlayerMap
-	if ActivePlayer == nil {
+	PlayerList = newPlayerList
+	if len(PlayerList) == 0 { // set player to nil if no players found
+		ActivePlayer = nil
 		return nil
 	}
 
-	if mapPlay, exists := PlayerMap[ActivePlayer.name]; exists {
-		ActivePlayer = mapPlay
-	} else {
-		ActivePlayer = play
-	}
+	PlayerNum = PlayerNum % len(PlayerList)
+	ActivePlayer = PlayerList[PlayerNum]
 
 	return nil
 }
